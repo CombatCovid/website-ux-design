cf5ca0634f37861e26adbc24555d55a45a052ac4
Jose Carlos Urra <jose.urra86@gmail.com>
2020-03-30 22:43:37 +0200

    First piece of wiring working with v-for directive

--- src/templates/Doc.vue
+++ src/templates/Doc.vue
@@ -1,6 +1,10 @@
 <template>
     <Layout style="padding-top:100px">
         <h1>Hey {{$context.name }}</h1>
+        <v-flex xs12 md3 v-for="repo in $page.gitapi.organization.repositories.nodes" :key="repo.name">
+            <p v-if="$context.name == repo.name">{{$context.name}}</p>
+        </v-flex>
+        </div>
     </Layout>
 
 </template>
@@ -10,6 +14,22 @@ export default {
     metaInfo:'hello World',
     meta:[
         {name:"author", content:"John Doe"}
-    ]
+    ],
 }
+   
 </script>
+
+<page-query>
+query Doc{
+    gitapi{
+        organization(login:"CombatCovid"){
+            repositories(first:50){
+                nodes{
+                  name
+                  nameWithOwner
+                }
+            }
+        }
+    }
+}
+</page-query>
