777aa0aa185d4db3bf0a0d732a7f128415ae63b6
Jose Carlos Urra <jose.urra86@gmail.com>
2020-04-01 14:39:58 +0200

    Cleaned Finder viewer with RepoList component

--- src/pages/Finder.vue
+++ src/pages/Finder.vue
@@ -18,25 +18,7 @@
 
     <v-container grid-list-lg fluid>
       <v-layout row wrap>
-        <v-flex xs12 md3 v-for="repo in $page.gitapi.organization.repositories.nodes" :key="repo.name">
-          <v-card
-            v-on:click="showContent(repo.name)"
-            hover min-height="350px" max-height="350px"
-          >
-            <v-img v-if="repo.images !== null"
-                   p-5 class="white--text align-end" height="200px"
-                   :src="getImgUrl(repo.nameWithOwner, repo.images.entries[0].name)"
-            >
-            </v-img>
-            <v-img v-else
-                   p-5 class="white--text align-end" height="200px"
-                   src="https://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder.png"
-            >
-            </v-img>
-            <v-card-title v-text="repo.name"></v-card-title>
-            <v-card-subtitle v-text="repo.description"></v-card-subtitle>
-          </v-card>
-        </v-flex>
+        <RepoList></RepoList>
       </v-layout>
     </v-container>
   </Layout>
@@ -46,8 +28,12 @@
 
   import { createSearchClient } from '@algolia/client-search'
   import algoliasearch from 'algoliasearch'
+  import RepoList from '~/components/RepoList.vue'
 
   export default {
+    components:{
+      RepoList
+    },
     metaInfo: {
       title: 'Demo Connect'
     },
