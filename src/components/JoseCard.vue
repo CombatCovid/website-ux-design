c64d5647b3f3e8501c2ddac813365db44a7ff744
Narration SD <narrationsd@gmail.com>
2020-04-01 23:51:10 -0700

    This is presenting well now.

--- src/components/JoseCard.vue
+++ src/components/JoseCard.vue
@@ -4,7 +4,7 @@
           hover min-height="350px" max-height="350px"
   >
     <v-img v-if="repo.cardImage !== null && typeof repo.cardImage !== 'undefined'"
-           p-5 class="white--text align-end" height="200px"
+           p-5 class="white--text align-end" height="200px"  min-width="200px" max-width="200px"
            :src="getImgUrl(repo.nameWithOwner, repo.cardImage)"
     >
     </v-img>
@@ -13,8 +13,8 @@
            src="/resources/image/image-placeholder.png"
     >
     </v-img>
-    <v-card-title v-text="repo.name"></v-card-title>
-    <v-card-subtitle v-text="repo.description"></v-card-subtitle>
+    <v-card-title v-text="repo.title" class="wrapping-text"></v-card-title>
+    <v-card-subtitle v-text="repo.description" class="wrapping-text"></v-card-subtitle>
   </v-card>
 
 </template>
@@ -38,19 +38,25 @@
     methods: {
       getImgUrl: function (nameWithOwner, fileName) {
         if (fileName !== null) {
-          console.log('fileName: ' + fileName)
-          console.log('cardImage path: ' + `https://raw.githubusercontent.com/${nameWithOwner}/master${fileName}`)
+          // console.log('fileName: ' + fileName)
+          // console.log('cardImage path: ' + `https://raw.githubusercontent.com/${nameWithOwner}/master${fileName}`)
           return `https://raw.githubusercontent.com/${nameWithOwner}/master${fileName}`
         } else {
           return "/resources/image/image-placeholder.png"
         }
       },
       showContent(repoName){
-        return this.$router.push({ path: `/viewer/${repoName}`, design: '${repoName}' })      }
+        if (repoName) {
+          return this.$router.push({ path: `/viewer/${repoName}`, design: `${repoName}` })      }
+        }
     }
   }
 </script>
 
 <style scoped>
+
+  .wrapping-text {
+    word-break: normal;
+  }
 
 </style>
--- src/pages/Finder.vue
+++ src/pages/Finder.vue
@@ -5,12 +5,15 @@
     <client-only>
       <ais-instant-search :index-name="indexName"
                           :search-client="searchClient" class="horiz-center searchbox">
+
+        <ais-configure :hits-per-page.camel="8" />
+
         <ais-powered-by/>
-        <ais-search-box reset-titled="Reset" :refresh="true"/>
+        <ais-search-box/>
 
         <ais-hits class="clear-above">
           <div slot-scope="{ items }">
-            <p>{{ JSON.stringify(items)}}</p>
+<!--            <p>{{ JSON.stringify(items)}}</p>-->
             <v-container grid-list-lg fluid>
               <v-layout d-flex flex-wrap>
                 <v-row>
@@ -25,33 +28,32 @@
           </div>
         </ais-hits>
 
-        <ais-configure :hitsPerPage="5" :page="1"/>
       </ais-instant-search>
     </client-only>
 
-    <v-container grid-list-lg fluid>
-      <v-layout row wrap>
-        <v-flex xs12 md3 v-for="repo in $page.gitapi.organization.repositories.nodes" :key="repo.name">
-          <v-card
-            v-on:click="showContent(repo.name)"
-            hover min-height="350px" max-height="350px"
-          >
-            <v-img v-if="repo.images !== null"
-                   p-5 class="white--text align-end" height="200px"
-                   :src="getImgUrl(repo.nameWithOwner, repo.images.entries[0].name)"
-            >
-            </v-img>
-            <v-img v-else
-                   p-5 class="white--text align-end" height="200px"
-                   src="https://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder.png"
-            >
-            </v-img>
-            <v-card-title v-text="repo.name"></v-card-title>
-            <v-card-subtitle v-text="repo.description"></v-card-subtitle>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+<!--    prior way...-->
+<!--    <v-container grid-list-lg fluid>-->
+<!--      <v-layout row wrap>-->
+<!--        <v-flex xs12 md3 v-for="repo in $page.gitapi.organization.repositories.nodes" :key="repo.name">-->
+<!--          <v-card-->
+<!--            v-on:click="showContent(repo.name)"-->
+<!--            hover min-height="350px" max-height="350px"-->
+<!--          >-->
+<!--            <v-img v-if="repo.images !== null"-->
+<!--                   p-5 class="white&#45;&#45;text align-end" height="200px"-->
+<!--                   :src="getImgUrl(repo.nameWithOwner, repo.images.entries[0].name)"-->
+<!--            >-->
+<!--            </v-img>-->
+<!--            <v-img v-else p-5 class="white&#45;&#45;text align-end" height="200px" max-width="200px"-->
+<!--                   src="https://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder.png"-->
+<!--            >-->
+<!--            </v-img>-->
+<!--            <v-card-title v-text="repo.name"></v-card-title>-->
+<!--            <v-card-subtitle v-text="repo.description"></v-card-subtitle>-->
+<!--          </v-card>-->
+<!--        </v-flex>-->
+<!--      </v-layout>-->
+<!--    </v-container>-->
   </Layout>
 </template>
 
@@ -100,58 +102,58 @@
 // this is hardwired, as api graphql requires a first: or last: value,
 // but I believe this isn't settable in Gridsome unless creating page
 // programatically, via createPage()
-<page-query>
-  query Algo1 {
-  gitapi{
-    organization(login:"CombatCovid"){
-      repositories(first:50){
-        nodes {
-          name
-          nameWithOwner
-          docs: object(expression: "master:docs") {
-            ... on GitApi_Tree {
-              folders: entries {
-                lang: name
-                ... FolderInfo
-              }
-            }
-           }
-           images: object(expression: "master:docs/img") {
-             ... on GitApi_Tree {
-               entries {
-                 name
-               }
-             }
-           }
-           srcs: object(expression: "master:src") {
-             ... on GitApi_Tree {
-               entries {
-                 name
-               }
-             }
-           }
-        }
-      }
-    }
-  }
-}
+<!--<page-query>-->
+<!--  query Algo1 {-->
+<!--  gitapi{-->
+<!--    organization(login:"CombatCovid"){-->
+<!--      repositories(first:50){-->
+<!--        nodes {-->
+<!--          name-->
+<!--          nameWithOwner-->
+<!--          docs: object(expression: "master:docs") {-->
+<!--            ... on GitApi_Tree {-->
+<!--              folders: entries {-->
+<!--                lang: name-->
+<!--                ... FolderInfo-->
+<!--              }-->
+<!--            }-->
+<!--           }-->
+<!--           images: object(expression: "master:docs/img") {-->
+<!--             ... on GitApi_Tree {-->
+<!--               entries {-->
+<!--                 name-->
+<!--               }-->
+<!--             }-->
+<!--           }-->
+<!--           srcs: object(expression: "master:src") {-->
+<!--             ... on GitApi_Tree {-->
+<!--               entries {-->
+<!--                 name-->
+<!--               }-->
+<!--             }-->
+<!--           }-->
+<!--        }-->
+<!--      }-->
+<!--    }-->
+<!--  }-->
+<!--}-->
 
-fragment FolderInfo on GitApi_TreeEntry {
-    contents: object {
-      ... on GitApi_Tree {
-        files: entries {
-          name
-          object {
-            ...on GitApi_Blob {
-              isBinary
-              text
-            }
-          }
-        }
-      }
-    }
-  }
-</page-query>
+<!--fragment FolderInfo on GitApi_TreeEntry {-->
+<!--    contents: object {-->
+<!--      ... on GitApi_Tree {-->
+<!--        files: entries {-->
+<!--          name-->
+<!--          object {-->
+<!--            ...on GitApi_Blob {-->
+<!--              isBinary-->
+<!--              text-->
+<!--            }-->
+<!--          }-->
+<!--        }-->
+<!--      }-->
+<!--    }-->
+<!--  }-->
+<!--</page-query>-->
 
 <style scoped>
   .horiz-center {
