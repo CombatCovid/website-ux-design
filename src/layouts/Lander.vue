b4d10f8dab6c36ac4a0afd8a053905e4d0c37042
Jose Carlos Urra <jose.urra86@gmail.com>
2020-04-04 12:02:03 +0200

    Added a lander layout

---
+++ src/layouts/Lander.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <slot/>
+    </div>
+</template>
+
+<static-query>
+  query {
+    metadata {
+              siteName
+    }
+  }
+</static-query>
+
+<script>
+  import Nav from '~/components/Nav.vue'
+
+  export default {
+    components: {
+      "Nav": Nav
+    }
+  }
+</script>
+
+<style>
+
+</style>
