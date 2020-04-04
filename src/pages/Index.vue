451260a946f041d1867e3639aaefb0aef1e2cd63
Jose Carlos Urra <jose.urra86@gmail.com>
2020-04-04 12:02:48 +0200

    Add parallax component for lander page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,34 @@
 <template>
   <div>
-    <Layout class="">
+    <Layout>
       <!-- Learn how to use images here: https://gridsome.org/docs/images -->
       <!-- <Carousel></Carousel> -->
+      <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="12">
+            <h1 class="display-1 font-weight-thin mb-4">Combat Covid with Hardware docs</h1>
+            <h4 class="subheading">Lets help documenting solutions</h4>
+          </v-col>
+        </v-row>
+      </v-parallax>
 
-      <h1>Hello, world!</h1>
+      <v-container  grid-list-xs>
+        <v-subheader>text</v-subheader>
+        <h1>Hello, world!</h1>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-        Dolores, aperiam non officia eos quod asperiores</p>
-
-      <p class="home-links">
-        <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-        <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-      </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
+          Dolores, aperiam non officia eos quod asperiores
+        </p>
+      </v-container>
     </Layout>
   </div>
 </template>
 
 <script>
-// import Carousel from "~/components/Carousel.vue";
+import Layout from "~/layouts/Lander.vue";
+
 export default {
   metaInfo: {
     title: "Home"
@@ -32,7 +41,7 @@ export default {
 
 <style>
 .home-links a {
-  padding-top:2em;
+  padding-top: 2em;
   margin-right: 1rem;
 }
 </style>
