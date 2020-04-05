308714a47cbafce484688852b68b2d04b9cec9f9
Narration SD <narrationsd@gmail.com>
2020-04-05 15:25:26 -0700

    current with integrate-jurra

--- src/pages/Documentation.vue
+++ src/pages/Documentation.vue
@@ -1,16 +1,105 @@
 <template>
   <Layout>
-    <div class="container">
-      <h1>Documentation</h1>
-      <p>...will go here...</p>
+    <div class="container doc-look">
+      <div class="content">
+        <h1>Documentation</h1>
+        <p>This is a very early stage of this page, and you will certainly see it grow.</p>
+        <p>For the moment, just one important area begins....</p>
+        <h2 id="prepare">Getting your Design Documents up on the App</h2>
+        <p>Basically, you'll be in touch with us to do this. We're friendly
+          communicators, and it won't be hard.</p>
+        <p>... the rest is an early and brief outline, for now! ...</p>
+        <h3>What Design Documents are, and where they go</h3>
+        <p>There's a specification for a simple folder layout, where you will place your set
+          of documents and illustrations, with simple summary items at top. .</p>
+        <p>You'll create this package on your own GitHub repository, following the project form.</p>
+        <p>Design texts are written in plain Markdown, which helps you and us get your structure
+          and formatting to come out easily. They go in the /docs folder.</p>
+        <p>Design images are in the normal web-compatible forms: jpg and png mainly,
+          and go in their own /docs/img folder, where they're easily included in Markdown,
+          and can also be rapidly scanned through when viewing on the app.</p>
+        <h4>How do you include what's on your own website(s)</h4>
+        <p>This is very important in our picture, as this is where you open your rich
+          further resources, and intentions, framings, and so forth as you wish,
+          for the Designs searcher to reference and understand.</p>
+        <p>We provide that you can use normal internet _ web links_ for this, simply
+          done anywhere in the design texts, and these will always open a fresh page
+          on the app user's browser, so they won't lose their place in discovering
+          your Design there when they click them.</p>
+        <h3>Uploading, once your initial package is ready</h3>
+        <p>This is very simple. You will make a normal Github 'pull request', (PR), to
+          the CombatCovid project repository, and after we've verified the contents,
+          it will go into that resource, and become immediately available throughout the
+          internet world, on web browsers from phones to desktops, where the app appears.
+        </p>
+        <h3>Then, how to upgrade, as you have more material for your Design?</h3>
+        <p>Again, this is going to be comfortable and suitably easy.</p>
+        <p>You'll do just as for the initial upload, place a PR with us, and we'll
+          make this your online results, with the care always in the normal process.</p>
+        <h2 id="metadata">Metadata, a real opportunity</h2>
+        <p>Metadata is the key to making it very easy for persons to discover your Design, and
+          discover it <em>appropriately to their needs</em>, through the app.</p>
+        <p>For the moment, it's good to be aware that you can specify your own apropos
+          search <em>keywords</em>by yourself -- and the Finder will intelligently use
+          them -- so you can have control.</p>
+        <p>You'll put these and other important Metadata in your Summary document,
+          in the usual Markdown (frontmatter) way, and that's one of the things
+          we'll give you a good explanation how to do.</p>
+        <h2>Approvals</h2>
+        <p>The quality of the Discovery Experience, for persons wanting to build
+          your Design out in the world, is the central important experience they'll have.</p>
+        <p>We can all appreciate that as these designs are for medical purposes, and
+          especially in the current crisis, that there needs to be oversight to enable
+          both ease and confidence.</p>
+        <p>We'll help you get your document repository into form, using the normal
+          and very well experienced Github PR process.</p>
+        <p>And we are committed to providing the best ease for you and your Design Documents,
+          in referencing professional medical and other (engineering, builder?) advice,
+          as well as connecting resources for matters such as ratings.
+          Details and capabilities will develop with our project.</p></div>
     </div>
   </Layout>
 </template>
 
 <script>
-export default {
-  metaInfo: {
-    title: 'Documentation'
+  export default {
+    metaInfo: {
+      title: 'Documentation'
+    }
   }
-}
 </script>
+
+<style scoped>
+
+  /*  Initially set after Vuetify grabs basics for these... */
+
+  body {
+    background-color: #e3f9fd;
+  }
+
+  h2, h3, h4 {
+    margin-bottom: 0.5rem;
+  }
+
+  p {
+    margin-left: 1rem;
+  }
+
+  .content {
+    background-color: #fdfde3;
+    padding: 20px;
+  }
+
+  .doc-look {
+    color: #164361;
+    font-family: Roboto, sans-serif;
+    font-size: small;
+    max-width: 640px; /* This is narrow for glance-readability...esp. eyeglass wearers */
+    margin-top: 1.5rem;
+    padding-left: 30px;
+  }
+
+  body {
+
+  }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,48 +1,110 @@
 <template>
   <div>
     <Layout class="">
-      <div class="formal-look">
-        <h2>Greetings</h2>
-        <br>
+      <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="12">
+            <h1 class="display-5 font-weight-bold mb-1">Combat Covid with Hardware</h1>
+            <h2 class="subheading font-weight-light">Find solutions and help us document more</h2>
+            <v-layout row wrap class="pt-6" justify-center>
+              <v-flex xs12 md3>
+                <v-btn to="/finder" class="spaced-btn"
+                       dark color="blue">Search for designs
+                </v-btn>
+              </v-flex>
+              <v-flex xs12 md3>
+                <v-btn to="/documentation#prepare" class="spaced-btn"
+                       dark color="orange">Upload designs
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-col>
+        </v-row>
+      </v-parallax>
 
-        <p>You're meeting the demonstration app of the Combat Covid with Hardware project.</p>
-        <p>This is a fast-moving Alpha production, so be reassured if you find that
-          items may move, appear, disappear, and at moments perhaps not function entirely
-          as you expected.</p>
-        <p>We're moving rapidly and constantly to add what's needed for early real use, and anything
-          unexpected will soon be repaired.</p><p>Thank you for your patience, and especially for your interest.</p>
-        <p class="sig-indent">The Combat Covid with Hardware Team</p>
-
-        <p class="home-links">
-          <!--        <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>-->
-          <!--        <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>-->
-        </p>
-      </div>
+      <v-container grid-list-xs class="home-container">
+        <v-layout row wrap>
+          <v-row xs-6 class="formal-look">
+            <h2 class="formal-intro">Greetings</h2>
+            <div class="formal-body">
+              <br>
+              <p>You're meeting the demonstration app of the Combat Covid with Hardware project.</p>
+              <p>This is a fast-moving Alpha production, so be reassured if you find that
+                items may move, appear, disappear, and at moments perhaps not function entirely
+                as you expected.</p>
+              <p>We're moving rapidly and constantly to add what's needed for early real use,
+                and anything unexpected will soon be repaired.</p>
+              <p>Thank you for your patience, and especially for your interest.</p>
+              <p class="sig-indent">The Combat Covid with Hardware Team</p>
+            </div>
+          </v-row>
+        </v-layout>
+      </v-container>
     </Layout>
   </div>
 </template>
 
 <script>
-export default {
-  metaInfo: {
-    title: "Home"
+  export default {
+    metaInfo: {
+      title: "Home"
+    }
   }
-};
 </script>
 
-<style>
-.home-links a {
-  padding-top:2em;
-  margin-right: 1rem;
-}
-.sig-indent {
-  margin-left: 24px;
-}
-.formal-look {
-  color: #1d5c87;
-  font-family: Roboto, sans-serif;
-  font-size: small;
-  padding: 20px;
-  max-width: 640px;
-}
+<style scoped>
+
+  @media only screen and (min-width: 959px) {
+    .home-container {
+      max-width: 960px;
+
+    }
+  }
+
+  @media only screen and (max-width: 959px) {
+    .spaced-btn {
+      margin: 0.3rem 0;
+    }
+  }
+
+  .home-links a {
+    padding-top: 2em;
+    margin-right: 1rem;
+  }
+
+  .sig-indent {
+    margin-left: 24px;
+  }
+
+  .formal-look {
+    color: #1d5c87;
+    font-family: Roboto, sans-serif;
+    font-size: small;
+    max-width: 640px; /* This is narrow for glance-readability...esp. eyeglass wearers */
+    margin-top: 1.5rem;
+    padding-left: 30px;
+  }
+
+  .formal-intro, .formal-title {
+    padding: 0 10px;
+  }
+
+  .formal-body {
+    padding: 0 30px;
+  }
+
+  @media only screen and (max-width: 640px) {
+    .formal-look {
+      padding-left: 10px;
+    }
+
+    .formal-intro, .formal-title {
+      padding: 0 5px;
+    }
+
+    .formal-body {
+      padding: 0 15px;
+    }
+  }
+
 </style>
