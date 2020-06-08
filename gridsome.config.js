5241e4cab9ec668b17e46c9b2abbb32ceb8d9544
Narration SD <narrationsd@gmail.com>
2020-06-08 13:46:36 -0700

    The real fix for the knotty missing-sliders problem is here. It was not in DesignDetail component; rather from a bad piece of javascript in gridsome.config.js, first of all an 'if' with no brackets, bad enough if not fatal. But then when it was active, in 'production', it called purgecss...on a config entirely wrong, for postcss. The result clearly stripped some built code, so that the sliders lost their layout very badly, hid their contents, and invisibly to us pretty much just what had happened. I've made the gridsome config totally as it needs to be now, taking out unused source-graphql plugin, no purgecss as that's tailwind-internal now (to great relief), and not incidentally, semi-colons out as we are ES6. I've left the errored purgecss code visible in comments, so we can observe later just what went wrong here. It will all go out in the real branches going forward, thus eventually end up back in develop here.

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,12 @@
 const tailwind = require('tailwindcss');
-const purgecss = require('@fullhuman/postcss-purgecss');
+// const purgecss = require('@fullhuman/postcss-purgecss');
 
 const postcssPlugins = [tailwind()];
 
-if (process.env.NODE_ENV === 'production')
-  postcssPlugins.push(purgecss(require('./postcss.config.js')));
+// if (process.env.NODE_ENV === 'production') {
+//   // postcssPlugins.push(purgecss(require('./postcss.config.js')));
+//   postcssPlugins.push(purgecss(require('./purgecss.config.js')));
+// }
 
 // This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
@@ -23,19 +25,8 @@ module.exports = {
         typeName: 'Documentation', // Required
         baseDir: './content/docs', // Where .md files are located
         pathPrefix: '/docs', // Add route prefix. Optional
-        template: './src/templates/Docs.vue', // Optional
-      },
-    },
-    {
-      use: '@gridsome/source-graphql',
-      options: {
-        url: 'https://api.github.com/graphql',
-        fieldName: 'gitapi',
-        typeName: 'GitApi',
-        headers: {
-          Authorization: 'Bearer ' + process.env.GRIDSOME_CC_SINGLE_AUTH,
-        },
-      },
+        template: './src/templates/Docs.vue' // Optional
+      }
     },
   ],
   css: {
@@ -45,10 +36,9 @@ module.exports = {
       },
     },
   },
-
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
     svgRule.uses.clear();
     svgRule.use('vue-svg-loader').loader('vue-svg-loader');
-  },
-};
+  }
+}
