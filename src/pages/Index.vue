17c79c9d9697f51ca0d4b9f7a32de47d44eb864d
Jose Carlos Urra <jose.urra86@gmail.com>
2020-07-08 15:16:20 +0200

    Fixed minor padding and layout issues in the index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <HomeLayout>
     <main class="w-screen bg-primary-100 bg-image">
-      <div class="container py-32 mx-auto md:flex pr-6">
+      <div class="container px-2 sm:px-0 mx-auto py-32  md:flex md:pr-6">
         <div class="flex-3 mt-20 md:mt-0">
           <h1
             class="text-white font-semibold text-3xl md:text-5xl"
@@ -44,7 +44,7 @@
       </div>
     </main>
 
-    <section class="container mx-auto py-20">
+    <section class="container px-2 sm:px-0 mx-auto py-20">
       <h2 class="text-center text-2xl md:text-3xl font-bold text-primary-75">HOW THE APP WORKS</h2>
 
       <hr class="mx-auto w-32 rounded-full mt-2 h-2 bg-primary-75" />
@@ -64,7 +64,7 @@
     
 
     <div class="bg-gray-15">
-      <section class="container py-20">
+      <section class="container px-2 sm:px-0 mx-auto py-20">
         <h2
           class="text-center text-2xl md:text-3xl font-bold text-primary-75"
         >HOW TO DOCUMENT A SOLUTION</h2>
@@ -82,7 +82,7 @@
       </section>
     </div>
 
-    <section class="container mx-auto py-20">
+    <section class="container px-2 sm:px-0 mx-auto py-20">
       <h2
         class="text-center text-2xl md:text-3xl font-bold text-accent-75"
       >CONTRIBUTE TO CODE AND MORE</h2>
@@ -97,7 +97,7 @@
     </section>
 
     <section class="relative py-20 bg-image w-full bg-primary-100">
-      <div class="container mx-auto text-center z-20">
+      <div class="container px-2 sm:px-0 mx-auto text-center z-20">
         <h2 class="text-2xl text-white font-bold">NOTE</h2>
         <p class="mt-6 text-white text-xl">
           You're meeting the demonstration app of the Combat Covid with Hardware
