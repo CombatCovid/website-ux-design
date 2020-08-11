37d512d1a49dce5fdd3dee6fcacd2e5b00b60dcb
Jose Carlos Urra <jose.urra86@gmail.com>
2020-08-11 12:01:22 +0200

    scoped markdown scss

--- src/components/DesignDetail.vue
+++ src/components/DesignDetail.vue
@@ -360,7 +360,7 @@ export default {
 };
 </script>
 
-<style lang="scss"  >
+<style lang="scss" scoped>
 @import "../sass/button.scss";
 /*
     here are the filter-translated equivalents for the converted Markdown:
